435f6481a2927999070b9a4159ca5c09968c631a
San <san0558@naver.com>
2021-02-28 00:52:08 +0900

    # tdd를 위한 예제코드 추가

--- mvc/test_mvc.py
+++ mvc/test_mvc.py
@@ -6,3 +6,12 @@ def multiply_by_two(x):
 
 def test_multiply_by_two():
     assert multiply_by_two(4) == 7
+
+import pytest
+@pytest.fixture
+def api():
+    app = create_app(config.test_config)
+    app.config['TESTING'] = True
+    api = app.test_client()
+
+    return api
