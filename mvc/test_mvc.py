12177a6271758d67528cc159055d3aed62b1715b
San <san0558@naver.com>
2021-02-28 00:56:16 +0900

    # mvc 핑테스트 성공

--- mvc/test_mvc.py
+++ mvc/test_mvc.py
@@ -1,17 +1,27 @@
-
-
+import os, sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+print(PARENT_DIR)
+sys.path.append(PARENT_DIR)
+from run import create_app
 def multiply_by_two(x):
     return x * 2
 
 
 def test_multiply_by_two():
-    assert multiply_by_two(4) == 7
+    assert multiply_by_two(4) == 8
 
 import pytest
+
 @pytest.fixture
 def api():
-    app = create_app(config.test_config)
+    app = create_app()
     app.config['TESTING'] = True
     api = app.test_client()
-
     return api
+
+def test_ping(api):
+    resp = api.get('/quick_flask/ping')
+    assert b'pong' in resp.data
