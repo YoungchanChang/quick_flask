65d1b7039b4b4e1487c2e840e093d002defb0d63
youngchan <san0558@naver.com>
2021-03-24 09:31:31 +0900

    # sign_up 기본 틀에 로그 메시지 추가

--- mvc/view.py
+++ mvc/view.py
@@ -37,14 +37,54 @@ def ping_front():
 @quick_flask.route('/sign_up', methods=['POST'])
 def sign_up():
     """회원가입이 확인 end-point"""
+    input_json = request.get_json()  # 아래꺼랑 함께 비교한다.
     input_json = json.loads(request.get_data().decode('utf-8'))
     user_name = input_json.get("user_name") # input, param에 대한 검증 / get으로 하면 여러개 검증하기 힘듬
+
+    svc_name = "/quick_flask_test"
+    return_json = {
+        "status_code": 500,
+        "message": "",
+        "model_name": "",
+        "sender": ""
+    }
+
     try:
         user_status =  UserControl.is_user_signed(user_name)
         data = {"success" : user_status}
+
+        if data:
+            return_json["status_code"] = 200
+            return_json["final_answer"] = kakao_answer
+            return_json["message"] = "success"
+            return_json["model_name"] = "quick_test_model"
+            return_json["sender"] = "youngchan"
+
+        # err_log_thread = threading.Thread(target=log.save_server_logs, args=(svc_name, input_json, return_json))
+        # err_log_thread.start()
         return make_response(json.dumps(data), 200)
+
+    # 상위 클래스에서
     except ValueException:
         return make_response(jsonify(success=False), 401)
+
+    # 예외 발생된 경우
+    except Exception:
+        error_msg = traceback.format_exc()
+        error_json = {
+            "status": {
+                "status_code": 500,
+                "status_type": "fail",
+                "status_msg": error_msg
+            }
+        }
+        # err_log_thread = threading.Thread(target=log.save_server_errors, args=(svc_name, input_json, error_msg))
+        # err_log_thread.start()
+
+        # 에러인 경우 중계서버에 에러 메시지 보낸다.
+        # return make_response(convert_to_Json_format(error_json), 500)
+        return make_response(jsonify(success=False), 401)
+
 
 # http://localhost:8080/quick_flask/render_page
 @quick_flask.route('/render_page', methods=['GET', 'POST'])
